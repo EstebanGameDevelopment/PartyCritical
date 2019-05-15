8825791a4fce503446c8fe935c75c8160258d24b
EstebanGameDevelopment <services@yourvrexperience.com>
2019-05-15 15:57:15 +0200

    Interface for the CameraController

---
+++ Scripts/Utils/ICameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace PartyCritical
+{
+    /******************************************
+    * 
+    * ICameraController
+    * 
+    * @author Esteban Gallardo
+    */
+    public interface ICameraController
+    {
+        // FUNCTIONS
+        Vector3 GetPositionLaser();
+        Vector3 GetForwardLaser();
+        Vector3 GetForwardPoint(float _distance);
+        GameObject CheckRaycastAgainst(params string[] _layers);
+        Vector3 GetCollisionPointOfLaser(params string[] _layerIgnore);
+    }
+}
