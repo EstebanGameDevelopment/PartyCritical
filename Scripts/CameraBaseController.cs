1fa92e4f4e14d13ee88ae39817e4947c27c4dc96
EstebanGameDevelopment <services@yourvrexperience.com>
2020-02-01 18:42:04 +0100

    When there is IAP on Unity Editor is running with gyroscope

--- Scripts/CameraBaseController.cs
+++ Scripts/CameraBaseController.cs
@@ -112,7 +112,11 @@ namespace PartyCritical
         {
             get {
 #if UNITY_EDITOR
+#if ENABLE_IAP
+                return true;
+#else
                 return m_enableGyroscope;
+#endif
 #else
 
                 if (EnableARCore && !m_enableVR)
